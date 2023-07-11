720788dfc40e55ca675450541f5062de33e37f84
Muhammad Hassan Shakeel <mhassan20896@gmail.com>
2023-07-11 11:02:07 +0500

    add logger

---
+++ lib/config/logs/logger.dart
@@ -0,0 +1,55 @@
+import 'dart:developer';
+
+abstract class Logger {
+  void onRequest(String path, String method,
+      {dynamic body,
+      Map<String, dynamic>? headers,
+      Map<String, dynamic>? queryParameters});
+
+  void onResponse(String path, String method,
+      {required int statusCode, dynamic body});
+
+  void onError(String path, String method,
+      {int? statusCode, dynamic error, dynamic stackTrace});
+}
+
+class LoggerV1 implements Logger {
+  @override
+  void onError(String path, String method,
+      {int? statusCode, error, stackTrace}) {
+    log('ERROR START');
+    log('$method $path');
+    log('$statusCode');
+    log('$error');
+    _logIfNotNull('$stackTrace');
+    log('ERROR END');
+  }
+
+  @override
+  void onRequest(String path, String method,
+      {body,
+      Map<String, dynamic>? headers,
+      Map<String, dynamic>? queryParameters}) {
+    log('REQUEST START');
+    log('$method $path');
+    _logIfNotNull('$headers');
+    _logIfNotNull('$queryParameters');
+    _logIfNotNull('$body');
+    log('REQUEST END');
+  }
+
+  @override
+  void onResponse(String path, String method, {required int statusCode, body}) {
+    log('RESPONSE START');
+    log('$method $path');
+    log('$statusCode');
+    _logIfNotNull('$body');
+    log('RESPONSE END');
+  }
+
+  void _logIfNotNull(dynamic value) {
+    if (value != null) {
+      log('$value');
+    }
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:molix/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MolixApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
